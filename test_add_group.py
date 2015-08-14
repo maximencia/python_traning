270ffce0e2d9f2fbf7d4248846639e9a04944ca6
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2015-08-14 17:02:03 +0300

    Выделить вспомогательные методы, чтобы повысить читаемость тестов (использовать для этого механизм рефакторинга)
    Создать вспомогательный класс для хранения свойств группы(см файл group.py)
    Использовать этот вспомогательный класс в тестах для инициализации тестовых данных и для передачи этих данных во вспомогательные методы

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import  unittest,datetime
+from group import Group
 
 
 def is_alert_present(wd):
@@ -23,32 +24,32 @@ class test_add_group(unittest.TestCase):
     def time_now(self):
         self.now_time = datetime.datetime.now()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_group_page(self, wd):
         # init group creation
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, Group):
         # fill group form
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("123_"+str(now_time))
+        wd.find_element_by_name("group_name").send_keys(Group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("123_"+str(now_time))
+        wd.find_element_by_name("group_header").send_keys(Group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("123")
+        wd.find_element_by_name("group_footer").send_keys(Group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -63,9 +64,18 @@ class test_add_group(unittest.TestCase):
     def test_test_add_group(self):
          wd = self.wd
          self.open_home_page(wd)
-         self.login(wd)
+         self.login(wd, username="admin",password="secret")
          self.open_group_page(wd)
-         self.create_group(wd)
+         self.create_group(wd, Group(name="123_" + str(now_time),header= "123_" + str(now_time),footer= "123"))
+         self.return_to_group_page(wd)
+         self.logout(wd)
+
+    def test_test_add_empty_group(self):
+         wd = self.wd
+         self.open_home_page(wd)
+         self.login(wd, username="admin",password="secret")
+         self.open_group_page(wd)
+         self.create_group(wd, Group(name="",header= "",footer= ""))
          self.return_to_group_page(wd)
          self.logout(wd)
 
