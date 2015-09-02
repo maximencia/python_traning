0f3e67870a99675a5608b790c5347924edefe223
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2015-09-02 16:23:57 +0300

    4_3 В Тест на модификацию группы добавлено сравнение списков.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -8,7 +8,15 @@ def test_modify_first_group(app):
     # если не существует ни одной группы - создаем
     if app.group.count() == 0:
          app.group.create(Group(name="Group for delete"))
+
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="modify_123_" + str(now_time),footer="Modify"))
+    group = Group(name="modify_123_" + str(now_time),footer="Modify")
+    # запомним идендификатор изменяемой группы, а группа то у нас первая по этому [0]
+    group.id = old_groups[0].id
+    # сам ТЕСТ на модификацию
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    #cnfhsq список обновим до модифицированных значений и сравним оба списка
+    old_groups[0]= group
+    assert sorted(old_groups,key=Group.id_or_max) == sorted(new_groups , key=Group.id_or_max)
