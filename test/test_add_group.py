492254c0f9c225a20cb26e306e341de0640ce73b
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2015-09-03 12:20:43 +0300

    4_4 В тесты с группами добавлена операция хеширования

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,17 +8,19 @@ now_time = datetime.datetime.now()
 def test_add_group(app):
     old_groups = app.group.get_group_list()
     group=Group(name="123_" + str(now_time),header= "123_" + str(now_time),footer= "123")
+    #[TEST]
     app.group.create(group)
+
+    assert len(old_groups)+1 == app.group.count() # хеш функция - считает количество элементов
     new_groups = app.group.get_group_list()
-    assert len(old_groups)+1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups,key=Group.id_or_max) == sorted(new_groups,key=Group.id_or_max)
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group=Group(name="",header="",footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups)+1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups,key=Group.id_or_max) == sorted(new_groups,key=Group.id_or_max)
+# def test_add_empty_group(app):
+#     old_groups = app.group.get_group_list()
+#     group=Group(name="",header="",footer="")
+#     app.group.create(group)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups)+1 == len(new_groups)
+#     old_groups.append(group)
+#     assert sorted(old_groups,key=Group.id_or_max) == sorted(new_groups,key=Group.id_or_max)
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -7,10 +7,7 @@ def test_delete_first_group(app):
       old_groups = app.group.get_group_list()
       #[TEST]
       app.group.delete_first_group()
-
+      assert len(old_groups)-1 == app.group.count() # хеш функция - считает количество элементов
       new_groups = app.group.get_group_list()
-      assert len(old_groups)-1 == len(new_groups)
       old_groups[0:1] = []
       assert old_groups == new_groups
-
-
