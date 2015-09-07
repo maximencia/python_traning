da8cc035af3eaa1ff19e3ffc3552e1974ed1a117
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2015-09-07 17:43:09 +0300

    FIX: убран уау

--- test/test_phones.py
+++ test/test_phones.py
@@ -15,7 +15,7 @@ def test_phone_on_home_page(app):
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.home == contact_from_edit_page.homeуау
+    assert contact_from_view_page.home == contact_from_edit_page.home
     assert contact_from_view_page.mobile == contact_from_edit_page.mobile
     assert contact_from_view_page.work == contact_from_edit_page.work
     assert contact_from_view_page.fax == contact_from_edit_page.fax
