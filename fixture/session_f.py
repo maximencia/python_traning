677f8b245c925bab9ce9497352e7a833c9c61f2f
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2015-09-07 13:49:27 +0300

    5_1 удалено использование бекслешей , упрощена функция is_logged_in_as

--- fixture/session_f.py
+++ fixture/session_f.py
@@ -13,11 +13,17 @@ class SessionHelper:
         # login
         self.app.fill_text_field(name="user",send_keys_parameters=username)
         self.app.fill_text_field(name="pass",send_keys_parameters=password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def is_logged_in_as(self,username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+        #return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"  #1 вариант
+        #return wd.find_element_by_xpath("//div/div[1]/form/b").text == "(%s)" % username # можно вот так
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]  # читает текст из окна браузера с круглыми скобочками которые мы отрезаем
 
     def ensure_login(self,username,password):
         wd = self.app.wd
