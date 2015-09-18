eb4f16fab8a4217e071e85b74b7724310b2a0552
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2015-09-18 17:51:02 +0300

    7_4 часть 1 замеряем скорость выгрузки из ui и bd
    0.323984470927 1 раз из ui
    0.565849749605 1000 раз из bd

--- test/test_db_match_ui.py
+++ test/test_db_match_ui.py
@@ -10,3 +10,13 @@ def test_group_list(app, db):
         return Group(id=group.id, name=group.name.strip())
     db_list=map(clean,db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+def test_group_list_2(app, db):
+    print(timeit(lambda: app.group.get_group_list(), number=1))
+    def clean(group):
+        return Group(id=group.id, name=group.name.strip())
+    print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
+    assert False #sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+    ..\..\..\..\..\..\python_traning\python_traning\test\test_db_match_ui.py 0.323984470927
+0.565849749605
